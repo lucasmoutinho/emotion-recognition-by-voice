713dd3d516a87223ead32b3a7892f605ce95f2bf
lucasmoutinho <lucas.silvamoutinho@gmail.com>
2019-10-08 15:49:51 -0300

    commnets

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pandas as pd # to read csv
 from sklearn.model_selection import train_test_split # to split dataset into train and test
+from sklearn import preprocessing
 
+# Read dataset
 data = pd.read_csv("dataset.csv")
 
 print(data.head())
 print(data.shape)
 
+# X is for features and y is for labels
 y = data.emotion
 X = data.drop('emotion', axis=1)
 
+# Split the ataset in train and test
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3)
 print("\nX_train:\n")
 print(X_train.head())
@@ -19,3 +23,8 @@ print("\nX_test:\n")
 print(X_test.head())
 print(X_test.shape)
 print(y_test.shape)
+
+# Binarize labels
+lb = preprocessing.LabelBinarizer()
+y_train = lb.fit_transform(y_train)
+y_test = lb.fit_transform(y_test)
