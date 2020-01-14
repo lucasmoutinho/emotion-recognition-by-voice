dee621a685ab895a2b6aabcf83800f4ececaa96c
lucasmoutinho <lucas.silvamoutinho@gmail.com>
2020-01-14 12:08:17 -0300

    created a first model for time series input

--- cnn_voice_model_3d.py
+++ cnn_voice_model_3d.py
@@ -15,6 +15,7 @@ from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split # to split dataset into train and test
 from sklearn import preprocessing
 from keras.callbacks import ReduceLROnPlateau,ModelCheckpoint
+from models.time_series_dataset_loader import TimeSeriesDatasetLoader
 # from imblearn.over_sampling import SMOTE
 
 
@@ -22,12 +23,10 @@ import sys
 sys.path.append("..")
 from scripts.prepare_data import standarization_unit_variance, normalize
 
-DATASET_PATH_X = "datasets/x_dataset.csv"
-DATASET_PATH_Y = "datasets/y_dataset.csv"
+DATASET_PATH = 'datasets/mfcc/'
 
 # Get dataset
-df_x = pd.read_csv(DATASET_PATH_X, sep=",")
-df_y = pd.read_csv(DATASET_PATH_Y, sep=",")
+dataset_loader = TimeSeriesDatasetLoader(DATASET_PATH)
 
 # cols = df.columns[df.columns.isin(['gender'])]
 # df = df[(df[cols] == 1).all(1)] # Only desired gender
@@ -41,29 +40,25 @@ df_y = pd.read_csv(DATASET_PATH_Y, sep=",")
 # df['emotion'] = np.where((df.emotion == 1) | (df.emotion == 2) | (df.emotion == 4) | (df.emotion == 6), 9, df.emotion) # negative emotions
 # df['emotion'] = df['emotion'] - 7
 
-# See dataset details
-print(df_x.head())
-print(df_x.shape)
-print(df_y.head())
-print(df_y.shape)
 # import pdb; pdb.set_trace()
 
 # split into input (X) and output (y) variables
-X = df_x[df_x.columns[-1]] # Only the MFCC features
-y = df_y[df_y.columns[-1]] # Emotion label
+X, y = dataset_loader.get_dataset()
+X = np.asarray(X)
+y = np.asarray(y)
 
 # # Normalization of input features in X
 # X = normalize(X)
 
 # See X and y details
-print(np.asarray(X[:3]))
+print(X[:3])
 print(X.shape)
 
 print(y[:3])
 print(y.shape)
 
-# # Split the dataset in train and test
-# X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3)
+# Split the dataset in train and test
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.3)
 
 # # # Applying SMOTE
 # # smt = SMOTE()
@@ -78,31 +73,31 @@ print(y.shape)
 # # X_train = X_train.reshape(X_train.shape[0],img_rows,img_cols,1).astype( 'float32' )
 # # X_test = X_test.reshape(X_test.shape[0],img_rows,img_cols,1).astype( 'float32' )
 
-# # # See Details
-# # print("\nX_train:\n")
-# # print(X_train[:3])
-# # print(X_train.shape)
+# See Details
+print("\nX_train:\n")
+print(X_train[:3])
+print(X_train.shape)
 
-# # print("\nX_test:\n")
-# # print(X_test[:3])
-# # print(X_test.shape)
+print("\nX_test:\n")
+print(X_test[:3])
+print(X_test.shape)
 
-# # print("\ny_train:\n")
-# # print(y_train[:3])
-# # print(y_train.shape)
+print("\ny_train:\n")
+print(y_train[:3])
+print(y_train.shape)
 
-# # print("\ny_test:\n")
-# # print(y_test[:3])
-# # print(y_test.shape)
+print("\ny_test:\n")
+print(y_test[:3])
+print(y_test.shape)
 
 # # # Binarize labels
 # # lb = preprocessing.LabelBinarizer()
 # # y_train = lb.fit_transform(y_train)
 # # y_test = lb.fit_transform(y_test)
 
-# # Create categorical matrices
-# y_train = to_categorical(y_train)
-# y_test = to_categorical(y_test)
+# Create categorical matrices
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
 
 # # See Details
 # print("\ny_train:\n")
@@ -121,34 +116,34 @@ print(y.shape)
 # X_testcnn = np.expand_dims(X_test, axis=2)
 # print(X_traincnn.shape)
 
-# # define the keras model
-# model = Sequential()
+# define the keras model
+model = Sequential()
 
-# model.add(Conv1D(128, 5,padding='same', input_shape=(10,1)))
-# model.add(Activation('sigmoid'))
-# model.add(Conv1D(64, 5,padding='same'))
-# model.add(Activation('sigmoid'))
-# model.add(Dropout(0.1))
-# model.add(MaxPooling1D(pool_size=(3)))
-# model.add(Conv1D(32, 5,padding='same',))
-# model.add(Activation('sigmoid'))
-# model.add(Conv1D(32, 5,padding='same',))
-# model.add(Activation('sigmoid'))
-# model.add(Flatten())
-# model.add(Dense(7))
-# model.add(Activation('softmax'))
-# # opt = keras.optimizers.rmsprop(lr=0.00001, decay=1e-6)
+model.add(Conv1D(128, 5,padding='same', input_shape=(10,1)))
+model.add(Activation('sigmoid'))
+model.add(Conv1D(64, 5,padding='same'))
+model.add(Activation('sigmoid'))
+model.add(Dropout(0.1))
+model.add(MaxPooling1D(pool_size=(3)))
+model.add(Conv1D(32, 5,padding='same',))
+model.add(Activation('sigmoid'))
+model.add(Conv1D(32, 5,padding='same',))
+model.add(Activation('sigmoid'))
+model.add(Flatten())
+model.add(Dense(7))
+model.add(Activation('softmax'))
+# opt = keras.optimizers.rmsprop(lr=0.00001, decay=1e-6)
 
 # # top-k category accuracy
 # top3_acc = functools.partial(keras.metrics.top_k_categorical_accuracy, k=3)
 # top3_acc.__name__ = 'top3_acc'
 
-# # compile the keras model
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', top3_acc])
+# compile the keras model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', top3_acc])
 
-# # Define bath and epochs
-# batch_size = 16
-# epochs = 500
+# Define bath and epochs
+batch_size = 16
+epochs = 500
 
 # # Fit model
 # #cnnhistory = model.fit(X_traincnn, y_train,
@@ -168,15 +163,15 @@ print(y.shape)
 # mcp_save = ModelCheckpoint('models/model_checkpoints/smote.h5', save_best_only=True, monitor='val_loss', mode='min')
 # cnnhistory=model.fit(X_traincnn, y_train, batch_size = batch_size, epochs = epochs, validation_data=(X_testcnn, y_test), callbacks=[mcp_save, lr_reduce])
 
-# # Model Summary
+# Model Summary
 
-# model.summary()
+model.summary()
 
-# # PLT History info
-# plt.plot(cnnhistory.history['loss'])
-# plt.plot(cnnhistory.history['val_loss'])
-# plt.title('model loss')
-# plt.ylabel('loss')
-# plt.xlabel('epoch')
-# plt.legend(['train', 'test'], loc='upper left')
-# plt.show()
+# PLT History info
+plt.plot(cnnhistory.history['loss'])
+plt.plot(cnnhistory.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
