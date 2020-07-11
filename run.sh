da71538681b58a8f6799a2685e2d0c807fe57985
Gabriel Almeida <g.almeidaa.campos@gmail.com>
2020-07-11 20:46:47 -0300

    add run sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+nohup python3 hyperopt_for_individual_emotion.py 0 &
+nohup python3 hyperopt_for_individual_emotion.py 1 &
+nohup python3 hyperopt_for_individual_emotion.py 2 &
+nohup python3 hyperopt_for_individual_emotion.py 3 &
+nohup python3 hyperopt_for_individual_emotion.py 4 &
+nohup python3 hyperopt_for_individual_emotion.py 5 &
+nohup python3 hyperopt_for_individual_emotion.py 6 &
