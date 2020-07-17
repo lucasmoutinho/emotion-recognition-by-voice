94d2916564702ce8634eb44326558d3582e7b5a4
Gabriel Almeida! <g.almeidaa.campos@gmail.com>
2020-07-17 20:27:40 +0000

    New hyperopt results

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-nohup python3 hyperopt_for_individual_emotion.py 0 &
-nohup python3 hyperopt_for_individual_emotion.py 1 &
-nohup python3 hyperopt_for_individual_emotion.py 2 &
-nohup python3 hyperopt_for_individual_emotion.py 3 &
-nohup python3 hyperopt_for_individual_emotion.py 4 &
-nohup python3 hyperopt_for_individual_emotion.py 5 &
-nohup python3 hyperopt_for_individual_emotion.py 6 &
+python3 hyperopt_for_individual_emotion.py 0
+python3 hyperopt_for_individual_emotion.py 1
+python3 hyperopt_for_individual_emotion.py 2
+python3 hyperopt_for_individual_emotion.py 3
+python3 hyperopt_for_individual_emotion.py 4
+python3 hyperopt_for_individual_emotion.py 5
+python3 hyperopt_for_individual_emotion.py 6 
